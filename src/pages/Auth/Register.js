deed63ab07bba5ed6829f2ab444cbc9c9d83a2fb
Thiago <thiagooffice2@gmail.com>
2022-09-26 18:48:37 -0300

    feat: add Redux

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -7,21 +7,54 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
   }
 
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
       <p class="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome"/>
-        <input type="email" placeholder="E-mail"/>
-        <input type="password" placeholder="Senha"/>
-        <input type="password" placeholder="Confirme a senha"/>
-        <input type="submit" placeholder="Cadastrar"/>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ""}
+        />
+        <input type="submit" placeholder="Cadastrar" />
       </form>
       <p>
         Já tem conta? <Link to="/login">Clique aqui.</Link>

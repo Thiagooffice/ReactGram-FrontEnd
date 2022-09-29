435ba079f3b8a83d16b704ec9a7b5fcd5cc82c1f
Thiago <thiagooffice2@gmail.com>
2022-09-29 19:06:37 -0300

    feat: Connecting form to reducer

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -6,11 +6,19 @@ import { Link } from "react-router-dom";
 //Hooks
 import { useState, useEffect } from "react";
 
+//Redux
+import { register, reset } from "../../slices/authSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch()
+
+  const {loading, error} = useSelector((state) => state.auth)
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,7 +31,14 @@ export default function Register() {
     };
 
     console.log(user);
+
+    dispatch(register(user))
   }
+
+  //Clean all auth states
+  useEffect(()=>{
+    dispatch(reset())
+  }, [dispatch])
 
   return (
     <div id="register">
@@ -54,7 +69,7 @@ export default function Register() {
           onChange={(e) => setConfirmPassword(e.target.value)}
           value={confirmPassword || ""}
         />
-        <input type="submit" placeholder="Cadastrar" />
+        <input type="submit" value="Cadastrar" />
       </form>
       <p>
         Já tem conta? <Link to="/login">Clique aqui.</Link>
